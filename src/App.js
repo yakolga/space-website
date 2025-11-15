2f876f5d7d8092eefa62cc501b421caf5b050a79
Olga Kulczycka <olga@webjaksklep.eu>
2025-11-15 16:07:49 +0100

    feat: hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Hero/>
     </div>
   );
 }
